0314a7f574caecffe2efc03e762b708d28240202
Sajjad Safari <safari.sajjaad@gmail.com>
2025-11-09 19:08:07 +0100

    supabase stuff updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,24 @@
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import LandingPageClient from '@/components/LandingPageClient'; // We will create this next
+import { createClient } from '@/lib/supabase/server';
+import { cookies } from 'next/headers';
 
-export default function Home() {
+export default async function Home() {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  const { data: todos } = await supabase.from('todos').select();
+
   return (
     <>
       <Header />
       <LandingPageClient />
+      <ul>
+        {todos?.map((todo) => (
+          <li key={todo.id}>{todo.title ?? JSON.stringify(todo)}</li>
+        ))}
+      </ul>
       <Footer />
     </>
   );
