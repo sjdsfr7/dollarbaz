97e003aa086f38d8bc01857abbb96598a370710f
Sajjad Safari <safari.sajjaad@gmail.com>
2025-11-14 22:22:01 +0100

    login action fixed

--- app/auth/login/actions.ts
+++ app/auth/login/actions.ts
@@ -3,10 +3,14 @@
 import { createClient } from '@/lib/supabase/server';
 import { redirect } from 'next/navigation';
 
-export const login = async (prevState: any, formData: FormData) => {
+type LoginState = {
+  error?: string;
+};
+
+export const login = async (prevState: LoginState, formData: FormData) => {
   const email = formData.get('email') as string;
   const password = formData.get('password') as string;
-  const supabase = createClient();
+  const supabase = await createClient();
 
   const { error } = await supabase.auth.signInWithPassword({
     email,
@@ -16,6 +20,7 @@ export const login = async (prevState: any, formData: FormData) => {
   if (error) {
     // Return a state object with the error message
     return {
+      ...prevState,
       error: 'Could not authenticate user. Please check your credentials.',
     };
   }
